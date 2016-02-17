b51fa266e5ec89dc81fd94d98250e2d8909f94a2
kuramameng <mengjs324@gmail.com>
2016-02-17 12:27:57 -0500

    create ajax.js file and add basic ajax calls to it

---
+++ js/ajax.js
@@ -0,0 +1,59 @@
+'use strict'
+var api = {
+  url: 'https://intense-tundra-9809.herokuapp.com',
+  //url: 'http://localhost:3000',
+  ajax: function(config, cb) {
+    $.ajax(config).done(function(data, textStatus, jqxhr) {
+      cb(null, data);
+    }).fail(function(jqxhr, status, error) {
+      cb({jqxher: jqxhr, status: status, error: error});
+    });
+  },
+
+  createPost: function(postInfo, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.url + '/v1/posts',
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(postInfo),
+      dataType: 'json'
+    }, callback);
+  },
+
+  listArchive: function(callback) {
+    this.ajax({
+      method: 'GET',
+      url: this.url + '/v1/posts',
+      dataType: 'json'
+    }, callback);
+  },
+
+  listPost: function(id, callback) {
+    this.ajax({
+      method: 'GET',
+      url: this.url + '/v1/posts' + id,
+      dataType: 'json'
+    }, callback);
+  },
+
+  updatePost: function(id, editInfo, callback) {
+    this.ajax({
+      method: 'PATCH',
+      url: this.url + '/v1/posts/' + id,
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(editInfo),
+      dataType: 'json'
+    }, callback);
+  },
+
+  deletePost: function(id, callback) {
+    this.ajax({
+      method: 'DELETE',
+      url: this.url + '/v1/posts/' + id,
+      headers: {
+        Authorization: 'Token token=' + token
+      },
+      dataType: 'json'
+    }, callback);
+  }
+}; // end of api object
