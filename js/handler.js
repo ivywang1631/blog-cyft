8ed56aaafbaabed35b4025c71aea7771ea9044b1
kuramameng <mengjs324@gmail.com>
2016-02-21 14:09:12 -0500

    show created date under each post and posts are sorted by dates

--- js/handler.js
+++ js/handler.js
@@ -32,8 +32,12 @@ function archiveHandler(){
   api.listArchive(function(error, data){
     if(error) console.error(error);
     $(".archive-content").html("");
-    data.posts.forEach(function(post){
-      $(".archive-content").append("<div class='post-preview' id='" + post.id + "'><a href='single_post.html'><h2 class='post-title'>" + post.title + "</h2></a><p class='post-meta'>" + post.text + "</div><div><a><div style='float: left;margin-right: 20px;' class='edit' id='edit-" + post.id + "'>Edit</div></a><a><div class='delete-post' id='delete-" + post.id + "'>Delete</div></a></div><hr>");
+    // sort posts by creation dates (descending)
+    var sortedPosts = data.posts.sort(function(a, b){
+      return new Date(b.created_at) - new Date(a.created_at);
+    });
+    sortedPosts.forEach(function(post){
+      $(".archive-content").append("<div class='post-preview' id='" + post.id + "'><a href='single_post.html'><h2 class='post-title'>" + post.title + "</h2></a><p class='post-meta'>" + post.text + "</div><div style='font-size: 14px; font-style: italic'>Created at: " + new Date(post.created_at) + "</div><div><a><div style='float: left;margin-right: 20px;' class='edit' id='edit-" + post.id + "'>Edit</div></a><a><div class='delete-post' id='delete-" + post.id + "'>Delete</div></a></div><hr>");
     });
     deleteHandler();
   })
