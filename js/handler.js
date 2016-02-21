072a1d4a78bbbe50194b52ceec63ac4dca9fd42b
kuramameng <mengjs324@gmail.com>
2016-02-21 15:53:31 -0500

    tweaking css styling and some final testings

--- js/handler.js
+++ js/handler.js
@@ -77,7 +77,7 @@ function archiveHandler(){
       return new Date(b.created_at) - new Date(a.created_at);
     });
     sortedPosts.forEach(function(post){
-      $(".archive-content").append("<div class='post-preview' id='" + post.id + "'><a href='single_post.html'><h2 class='post-title'>" + post.title + "</h2></a><p class='post-meta'>" + post.text + "</div><div style='font-size: 14px; font-style: italic'>Created at: " + new Date(post.created_at) + "</div><div><a><div style='float: left;margin-right: 20px;' class='edit' id='edit-" + post.id + "'>Edit</div></a><a><div class='delete-post' id='delete-" + post.id + "'>Delete</div></a></div><hr>");
+      $(".archive-content").append("<div class='post-preview' id='" + post.id + "'><h2 class='post-title'>" + post.title + "</h2><p class='post-meta'>" + post.text + "</div><div style='font-size: 14px; font-style: italic'>Created at: " + new Date(post.created_at) + "</div><div><a><div style='float: left;margin-right: 20px;' class='edit' id='edit-" + post.id + "'>Edit</div></a><a><div class='delete-post' id='delete-" + post.id + "'>Delete</div></a></div><hr>");
     });
     deleteHandler();
     editHandler();
