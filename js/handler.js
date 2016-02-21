30b25ca21dd38db338c137e034df535e32c4cc87
kuramameng <mengjs324@gmail.com>
2016-02-21 12:24:33 -0500

    make create post functionality working

--- js/handler.js
+++ js/handler.js
@@ -9,6 +9,13 @@ function form2object(form) {
   return data;
 };
 
+function wrap(root, formData) {
+  var wrapper = {};
+  wrapper[root] = formData;
+  return wrapper;
+};
+
+
 function archiveHandler(){
   archiveShow();
   // loop through all posts for view rendering
@@ -38,12 +45,13 @@ $(document).ready(function(){
   $(".post-form").on("submit",function(e){
     e.preventDefault();
     // JSON constructor
-    var postInfo = form2object(this);
-    // create post
+    var postInfo = wrap('v1_post', form2object(this));
+    //create post
     api.createPost(postInfo, function(error, data){
       if(error) console.error(error);
+      console.log(data);
       // redirect user to archive page after post creation & populate views
-      archiveHandler();
+      //archiveHandler();
     })
 
   })
